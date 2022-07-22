ef210219b38c4de8952d589853d77a9e006e6d1b
Abigail Bartolome <abijbart@vt.edu>
2022-07-22 18:47:36 -0400

    linking

--- src/App.js
+++ src/App.js
@@ -45,21 +45,21 @@ function App() {
       </header>
       <div className="App-container">
         <div className="App-menu">
-        <Link className={current === 4 ? 'active' : ''} to="/" onClick={() => setCurrent(4)}>
+        <Link className={current === 4 ? 'active' : ''} to="/glucovisPrototype/home" onClick={() => setCurrent(4)}>
           <HomeOutlined style={{color: '#7A3FD7' }}/> Home
           </Link>
           <h1>CHARTS</h1>
-          <Link className={current === 0 ? 'active' : ''} to="/agp" onClick={() => setCurrent(0)}>
+          <Link className={current === 0 ? 'active' : ''} to="/glucovisPrototype/agp" onClick={() => setCurrent(0)}>
             <AreaChartOutlined style={{color: '#9655FF', lineHeight: '48px', padding: '0px 10px', verticalAlign:'text-bottom'}}/> AGP
           </Link>
-          <Link className={current === 1 ? 'active' : ''} to="/overlay" onClick={() => setCurrent(1)}>
+          <Link className={current === 1 ? 'active' : ''} to="/glucovisPrototype/overlay" onClick={() => setCurrent(1)}>
             <LineChartOutlined style={{color: '#9655FF', lineHeight: '48px', padding: '0px 10px', verticalAlign:'text-top'}}/> Overlay
           </Link>
-          <Link className={current === 2 ? 'active' : ''} to="/glucomine" onClick={() => setCurrent(2)}>
+          <Link className={current === 2 ? 'active' : ''} to="/glucovisPrototype/glucomine" onClick={() => setCurrent(2)}>
             <PicLeftOutlined style={{color: '#9655FF', lineHeight: '48px', padding: '0px 10px', verticalAlign:'text-top'}}/> GlucoMine
           </Link>
           <div className='App-menu-bottom'>
-            <Link className={current === 3 ? 'active' : ''} to="/about" onClick={() => setCurrent(3)}>
+            <Link className={current === 3 ? 'active' : ''} to="/glucovisPrototype/about" onClick={() => setCurrent(3)}>
               <QuestionCircleOutlined style={{color: '#9655FF', lineHeight: '48px', padding: '0px 10px', verticalAlign:'text-top'}}/> About
             </Link>
           </div>
@@ -67,11 +67,11 @@ function App() {
         <div className="App-content">
           <Context.Provider value={person}>
             <Routes>
-              <Route path="/home" element={<LandingPage />} />
-              <Route path="/agp" element={<AgpView />} />
-              <Route path="/overlay" element={<OverlayView />} />
-              <Route path="/glucomine" element={<GlucoMineView />} />
-              <Route path="/about" element={<AboutView />} />
+              <Route path="/glucovisPrototype/home" element={<LandingPage />} />
+              <Route path="/glucovisPrototype/agp" element={<AgpView />} />
+              <Route path="/glucovisPrototype/overlay" element={<OverlayView />} />
+              <Route path="/glucovisPrototype/glucomine" element={<GlucoMineView />} />
+              <Route path="/glucovisPrototype/about" element={<AboutView />} />
             </Routes>
           </Context.Provider>
         </div>
